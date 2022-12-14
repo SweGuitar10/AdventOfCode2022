89f96a8f929733ec942778f45e0cbc759bdcf427
SweGuitar10 <kurtare95@gmail.com>
2022-12-14 21:58:22 +0100

    apparently there's a part two

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <queue>
+#include <vector>
 
 int main()
 {
@@ -18,22 +19,31 @@ int main()
 	int sum = 0;
 
 	// Read each line until EOF is met
+	// Add each elves calories to a priority queue (max)
 	while (!file.eof())
 	{
 		std::getline(file, line);
 		sum += std::stoi(line);
 		
 		// When next line starts with \n it's presumed to be empty
-		if (file.peek() == '\n')
+		if (file.peek() == '\n' || file.peek() == EOF)
 		{
 			elfCalories.push(sum);
 			sum = 0;
 			std::getline(file, line); // remove '\n'
 		}
-
 	}
 
 	int largest = elfCalories.top();
 	std::cout << "Largest total of calories: " << largest << std::endl; 
+	
+	int top3sum = 0;
+	for (size_t i = 0; i < 3; i++)
+	{
+		top3sum += elfCalories.top();
+		elfCalories.pop();
+	}
+	
+	std::cout << "Top three elves' calories summed: " << top3sum <<std::endl;
 	return 0;
 }
